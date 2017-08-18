b2ecec88291a110ea4c4cb7d1930b46271a12040
plubojacky <plubojacky@email.com>
2017-08-18 14:14:36 -0700

    Install bootstrap css and router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div>
         
       </div>
+      </BrowserRouter>
     );
   }
 }
