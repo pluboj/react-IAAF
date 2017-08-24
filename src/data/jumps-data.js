e6a89ac6fcffb40cd420effd0cc2160ec2fa84cb
plubojacky <plubojacky@email.com>
2017-08-24 11:00:15 -0700

    Create jumps-data.js

---
+++ src/data/jumps-data.js
@@ -0,0 +1,20 @@
+export const HighJumpM = [
+	{
+		pos: "1",
+		name: "Mutaz Essa Barshim",
+		country: "QAT",
+		mark: "2.35"
+	},
+	{
+		pos: "2",
+		name: "Danil Lysenko",
+		country: "ANA",
+		mark: "2.32"
+	},
+	{
+		pos: "3",
+		name: "Majd Eddin Ghazal",
+		country: "SYR",
+		mark: "2.29"
+	}
+];
