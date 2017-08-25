8ee665fe5239703cdaca66f0d00c584d6cec9691
plubojacky <plubojacky@email.com>
2017-08-24 17:27:34 -0700

    Update jumps-data.js

--- src/data/jumps-data.js
+++ src/data/jumps-data.js
@@ -18,3 +18,24 @@ export const HighJumpM = [
 		mark: "2.29"
 	}
 ];
+
+export const HighJumpW = [
+	{
+		pos: "1",
+		name: "Maria LASITSKENE",
+		country: "ANA",
+		mark: "2.03"
+	},
+	{
+		pos: "2",
+		name: "Yuliia LEVCHENKO",
+		country: "UKR",
+		mark: "2.01"
+	},
+	{
+		pos: "3",
+		name: "Kamila LICWINKO",
+		country: "POL",
+		mark: "1.99"
+	}
+];
