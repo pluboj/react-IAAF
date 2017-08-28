2024567b96abacf0e05563444a7884f7c0235b43
plubojacky <plubojacky@email.com>
2017-08-28 11:15:48 -0700

    Update throws-data.js

--- src/data/throws-data.js
+++ src/data/throws-data.js
@@ -73,7 +73,26 @@ export const DiscusThrowM = [
 	}
 ];
 
-export const DiscusThrowW = [];
+export const DiscusThrowW = [
+	{
+		pos: "1",
+		name: "Sandra Perkovic",
+		country: "CRO",
+		mark: "70.31"
+	},
+	{
+		pos: "2",
+		name: "Dani Stevens",
+		country: "AUS",
+		mark: "69.64"
+	},
+	{
+		pos: "3",
+		name: "Mélina Robert-Michon",
+		country: "FRA",
+		mark: "66.21"
+	}
+];
 
 export const HammerThrow = [
 	"Athletes throw a metal ball (16lb/7.26kg for men, 4kg/8.8lb for women) for distance that’s attached to a grip by a steel wire no longer than 1.22m while remaining inside a seven-foot (2.135m) diameter circle.",
@@ -81,9 +100,47 @@ export const HammerThrow = [
 	"The thrower usually makes three or four spins before releasing the ball. Athletes will commonly throw four or six times per competition. In the event of a tie, the winner will be the athlete with the next-best effort."
 ];
 
-export const HammerThrowM = [];
+export const HammerThrowM = [
+	{
+		pos: "1",
+		name: "Pawel Fajdek",
+		country: "POL",
+		mark: "79.81"
+	},
+	{
+		pos: "2",
+		name: "Valeriy Pronkin",
+		country: "ANA",
+		mark: "78.16"
+	},
+	{
+		pos: "3",
+		name: "Wojciech Nowicki",
+		country: "POL",
+		mark: "78.03"
+	}
+];
 
-export const HammerThrowW = [];
+export const HammerThrowW = [
+	{
+		pos: "1",
+		name: "Anita Włodarczyk",
+		country: "POL",
+		mark: "77.90"
+	},
+	{
+		pos: "2",
+		name: "Zheng Wang",
+		country: "CHN",
+		mark: "75.98"
+	},
+	{
+		pos: "3",
+		name: "Malwina Kopron",
+		country: "POL",
+		mark: "74.76"
+	}
+];
 
 export const JavelinThrow = [
 	"Using one arm, a metal-tipped javelin is thrown as far as possible. The athlete must hold the javelin by its corded grip with his or her little finger closest to the tip of the implement.",
@@ -92,6 +149,44 @@ export const JavelinThrow = [
 	"If the tip touches the ground first, the throw is measured from this point. Athletes will commonly throw four or six times per competition. In the event of a tie, the winner will be the athlete with the next-best effort."
 ];
 
-export const JavelinThrowM = [];
+export const JavelinThrowM = [
+	{
+		pos: "1",
+		name: "Johannes Vetter",
+		country: "GER",
+		mark: "89.90"
+	},
+	{
+		pos: "2",
+		name: "Jakub Vadlejch",
+		country: "CZE",
+		mark: "89.73"
+	},
+	{
+		pos: "3",
+		name: "Petr Frydrych",
+		country: "CZE",
+		mark: "88.32"
+	}
+];
 
-export const JavelinThrowW = [];
+export const JavelinThrowW = [
+	{
+		pos: "1",
+		name: "Barbora Špotáková",
+		country: "CZE",
+		mark: "66.76"
+	},
+	{
+		pos: "2",
+		name: "Lingwei Li",
+		country: "CHN",
+		mark: "66.25"
+	},
+	{
+		pos: "3",
+		name: "Huihui Lyu",
+		country: "CHN",
+		mark: "65.26"
+	}
+];
